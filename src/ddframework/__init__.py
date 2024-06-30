be2b48a254dd29633b6c7866604acb78657fb72c
Michael Lamertz <michael.lamertz@gmail.com>
2024-06-30 03:36:43 +0200

    Renamed textgrid to screengrid

--- src/ddframework/__init__.py
+++ src/ddframework/__init__.py
@@ -1,13 +1,13 @@
 from .app import App, GameState, StateExit, StackPermissions
 from .countdownlabel import CountdownLabel
 from .statemachine import StateMachine, NoRoot, OpenGraph, UnknownNode, UnknownFollowupIndex
-from .textgrid import ScreenGrid
+from .screengrid import ScreenGrid, debug_grid
 from .textlabel import TextLabel, LabelStyle
 
 __all__ = [
     App, GameState, StateExit, StackPermissions,
     CountdownLabel,
     StateMachine, NoRoot, OpenGraph, UnknownNode, UnknownFollowupIndex,
-    ScreenGrid,
+    ScreenGrid, debug_grid,
     TextLabel, LabelStyle,
 ]
