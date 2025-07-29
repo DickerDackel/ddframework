90a6223f55d70ae34ed38babaf12b29b2cb05111
Michael Lamertz <michael.lamertz@gmail.com>
2025-07-29 11:54:41 +0200

    Try to get rid of warnings from stronger linting

---
+++ src/ddframework/__init__.pyi
@@ -0,0 +1,18 @@
+from .app import App as App
+from .app import GameState as GameState
+from .app import StateExit as StateExit
+from .app import StackPermissions as StatePermissions
+
+from .camera import Camera as Camera
+from .camera import CameraGroup as CameraGroup
+from .dynamicsprite import DynamicSprite as DynamicSprite
+from .dynamicsprite import RSAP as RSAP
+from .gridlayout import GridLayout as GridLayout
+from .gridlayout import debug_grid as grid
+from .statemachine import StateMachine as StateMachine
+from .statemachine import NoRoot as NoRoot
+from .statemachine import OpenGraph as OpenGraph
+from .statemachine import UnknownNode as UnknownNode
+from .statemachine import UnknownFollowupIndex as UnknownFollowupIndex
+from .textbox import LabelStyle as LabelStyle
+from .textbox import TextBox as TextBox
